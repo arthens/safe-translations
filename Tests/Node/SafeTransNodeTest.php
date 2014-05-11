a111503ee184be2b4ad83e2438983b1c02c60d08
Giacomo Gatelli <arthens@gmail.com>
2014-05-12 01:05:54 +1000

    Refactor and fix test

--- Tests/Node/SafeTransNodeTest.php
+++ Tests/Node/SafeTransNodeTest.php
@@ -16,23 +16,14 @@ class SafeTransNodeTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(sprintf(
                 'echo $this->env->getExtension(\'translator\')->getTranslator()->trans("Hello %%name%%", array_merge(array("%%name%%" => twig_escape_filter($this->env, %s)), %s), "messages");',
-                $this->getVariableGetterWithoutStrictCheck('name'),
-                $this->getVariableGetterWithStrictCheck('foo')
+                $this->getVariableGetter('name'),
+                $this->getVariableGetter('foo')
             ),
             trim($compiler->compile($node)->getSource())
         );
     }
 
-    protected function getVariableGetterWithoutStrictCheck($name)
-    {
-        if (version_compare(phpversion(), '5.4.0RC1', '>=')) {
-            return sprintf('(isset($context["%s"]) ? $context["%s"] : $this->getContext($context, "%s"))', $name, $name, $name);
-        }
-
-        return sprintf('$this->getContext($context, "%s", true)', $name);
-    }
-
-    protected function getVariableGetterWithStrictCheck($name)
+    protected function getVariableGetter($name)
     {
         if (version_compare(phpversion(), '5.4.0RC1', '>=')) {
             return sprintf('(isset($context["%s"]) ? $context["%s"] : $this->getContext($context, "%s"))', $name, $name, $name);
